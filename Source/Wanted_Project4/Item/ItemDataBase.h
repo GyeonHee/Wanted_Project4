af804e5430c3752380cc752b9b9332a50e205d82
GyeonHee <hyungi4833@gmail.com>
2025-11-05 02:42:16 +0900

    GAS System 등록 후 Item 2개 생성
    - GameplayAbility 플러그인 등록
    - 아이템 태그 등록 후, PrimaryDataAsset 상속 받은 ItemDataBase 클래스 구현
    - DataAsset으로 Sword, HealthPotion 임시 생성

---
+++ Source/Wanted_Project4/Item/ItemDataBase.h
@@ -0,0 +1,60 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
+#include "ItemDataBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WANTED_PROJECT4_API UItemDataBase : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+    /** PrimaryAssetId는 UPrimaryDataAsset일 때 필요 (AssetManager용) */
+    virtual FPrimaryAssetId GetPrimaryAssetId() const override
+    {
+        return FPrimaryAssetId(GetClass()->GetFName(), GetFName());
+    }
+
+    /** 태그 확인용 헬퍼 함수 */
+    UFUNCTION(BlueprintCallable, Category = "Item|Tags")
+    bool HasTag(FGameplayTag Tag) const
+    {
+        return ItemTags.HasTag(Tag);
+    }
+
+    UFUNCTION(BlueprintCallable, Category = "Item|Tags")
+    bool HasAnyTag(const FGameplayTagContainer& OtherTags) const
+    {
+        return ItemTags.HasAny(OtherTags);
+    }
+
+    UFUNCTION(BlueprintCallable, Category = "Item|Tags")
+    bool HasAllTags(const FGameplayTagContainer& OtherTags) const
+    {
+        return ItemTags.HasAll(OtherTags);
+    }
+
+protected:
+    // 아이템 이름 (표시용)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item")
+    FText ItemName;
+
+    // 아이템 아이콘
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item")
+    TSoftObjectPtr<UTexture2D> Icon;
+
+    // 태그 (여기에 Item.Equipment.Weapon.Sword 같은걸 넣음)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item|Tags")
+    FGameplayTagContainer ItemTags;
+
+    // 스택 수량
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item|Stats")
+    int32 MaxStackCount = 99;
+};
--- Source/Wanted_Project4/Wanted_Project4.Build.cs
+++ Source/Wanted_Project4/Wanted_Project4.Build.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
 
@@ -8,9 +8,20 @@ public class Wanted_Project4 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] 
+		{ 
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+            "GameplayTags",
+			"GameplayTasks"
+		});
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PublicIncludePaths.AddRange(new string[] { "Wanted_Project4" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
