89a041180017d9f04c4fb395536ac1b2926229c6
hanseoungheon <nick501040@gmail.com>
2025-11-06 18:24:04 +0900

    P4RPG 팀플 - 무한 맵 구현 완료. - 맵 파츠B 추가완료.

---
+++ Source/Wanted_Project4/Map/P4StageGimmck.cpp
@@ -0,0 +1,207 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Map/P4StageGimmck.h"
+#include "Kismet/GameplayStatics.h"
+#include "Character/P4CharacterBase.h"
+#include "Math/Vector.h"
+#include "Math/UnrealMathUtility.h"
+// Sets default values
+AP4StageGimmck::AP4StageGimmck()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AP4StageGimmck::BeginPlay()
+{
+	Super::BeginPlay();
+	
+
+	Vx = TileOffsetCoordB - TileOffsetCoordA;
+	Vy = TileOffsetCoordC - TileOffsetCoordA;
+
+	Vx.Z = 0.f;
+	Vy.Z = 0.f;
+
+	//플레이어 가져오기.
+	//AP4CharacterBase* PlayerCharacter 
+	//	= Cast<AP4CharacterBase>(UGameplayStatics::GetPlayerPawn(this,0));
+	// 일단 임시로 테스트를 위해 이걸로 설정.
+	APawn* PlayerCharacter 
+		= Cast<APawn>(UGameplayStatics::GetPlayerPawn(this,0));
+
+	if (PlayerCharacter != nullptr)
+	{
+		CurrentCenter = WorldToCell(PlayerCharacter->GetActorLocation());
+	}
+
+	RefreshTiles();
+}
+
+// Called every frame
+void AP4StageGimmck::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	APawn* PlayerCharacter
+		= Cast<APawn>(UGameplayStatics::GetPlayerPawn(this, 0));
+
+	if (PlayerCharacter == nullptr)
+	{
+		return;
+	}
+
+	const FIntPoint NewCenter = WorldToCell(PlayerCharacter->GetActorLocation());
+
+	if (NewCenter != CurrentCenter)
+	{
+		CurrentCenter = NewCenter;
+		RefreshTiles();
+	}
+}
+
+FIntPoint AP4StageGimmck::WorldToCell(const FVector& world) const
+{
+	const FVector Relative = world - GetActorLocation();
+
+	const FVector2D vx = FVector2D(Vx.X, Vx.Y);
+	const FVector2D vy = FVector2D(Vy.X, Vy.Y);
+	const FVector2D PlayerXY = FVector2D(Relative.X, Relative.Y);
+
+
+	const float det = vx.X * vy.Y - vx.Y * vy.X;
+
+	if (FMath::IsNearlyZero(det) == true)
+	{
+		const float SafeVx = FMath::IsNearlyZero(Vx.X) ? 1.0f : Vx.X;
+		const float SafeVy = FMath::IsNearlyZero(Vy.Y) ? 1.0f : Vy.Y;
+
+		int32 i = FMath::FloorToInt(Relative.X / SafeVx);
+		int32 j = FMath::FloorToInt(Relative.Y / SafeVy);
+		return { i,j };
+	}
+	
+	const float invDet = 1.0f / det;
+
+	const float iFloat = (PlayerXY.X * vy.Y - PlayerXY.Y * vy.X) * invDet;
+	const float jFloat = (-PlayerXY.X * vx.Y + PlayerXY.Y * vx.X) * invDet;
+
+
+	return FIntPoint(FMath::FloorToInt(iFloat), FMath::FloorToInt(jFloat));
+
+}
+
+FVector AP4StageGimmck::CellToWorld(const FIntPoint& cell) const
+{
+	//셀 기반 맵파츠 A의 위치.
+	FVector Pos = GetActorLocation() + (Vx * cell.X) + (Vy * cell.Y);
+
+	switch (GetCellType(cell))
+	{
+	case ETileType::A:
+		Pos += TileOffsetCoordA;
+		break;
+
+	case ETileType::B:
+		Pos += TileOffsetCoordB;
+		break;
+
+	case ETileType::C:
+		Pos += TileOffsetCoordC;
+		break;
+
+	case ETileType::D:
+		Pos += TileOffsetCoordD;
+		break;
+	}
+
+	return Pos;
+}
+
+void AP4StageGimmck::RefreshTiles()
+{
+	TSet<FIntPoint> Wanted;
+
+	for (int32 j = -GridRadiusY; j <= GridRadiusY; ++j)
+	{
+		for (int32 i = -GridRadiusX; i <= GridRadiusX; ++i)
+		{
+			const FIntPoint Cell = CurrentCenter + FIntPoint(i, j);
+			Wanted.Add(Cell);
+			SpawnTileAt(Cell);
+		}
+	}
+
+	TArray<FIntPoint> Keys;
+	ActiveTilesMap.GenerateKeyArray(Keys);
+
+	for (const FIntPoint& K : Keys)
+	{
+		if (Wanted.Contains(K) == false)
+		{
+			if (AActor* MapActor = ActiveTilesMap[K].Get())
+			{
+				MapActor->Destroy();
+			}
+
+			ActiveTilesMap.Remove(K);
+		}
+	}
+}
+
+AActor* AP4StageGimmck::SpawnTileAt(const FIntPoint& cell)
+{
+	if (TWeakObjectPtr<AActor>* Found = ActiveTilesMap.Find(cell))
+	{
+		if (Found->IsValid() == true)
+		{
+			return Found->Get();
+		}
+	}
+
+	TSubclassOf<AActor> Cls = PickTileClass(cell);
+
+	if (Cls == nullptr)
+	{
+		return nullptr;
+	}
+	
+	const FVector NewLocation = CellToWorld(cell);
+	const FRotator NewRotation = FRotator::ZeroRotator;
+
+	FActorSpawnParameters Params;
+	Params.Owner = this;
+
+	AActor* SpawnedMapActor = GetWorld()->SpawnActor<AActor>(Cls, NewLocation, NewRotation, Params);
+
+	if (SpawnedMapActor != nullptr)
+	{
+		ActiveTilesMap.Add(cell, SpawnedMapActor);
+	}
+	return SpawnedMapActor;	
+}
+
+TSubclassOf<AActor> AP4StageGimmck::PickTileClass(const FIntPoint& cell) const
+{
+	switch (GetCellType(cell))
+	{
+	case ETileType::A:
+		return TileA;
+
+	case ETileType::B:
+		return TileB;
+
+	case ETileType::C:
+		return TileC;
+
+	case ETileType::D:
+		return TileD;
+
+	default:
+		return TileD;
+	}
+}
+
---
+++ Source/Wanted_Project4/Map/P4StageGimmck.h
@@ -0,0 +1,125 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "P4StageGimmck.generated.h"
+
+UENUM(BlueprintType)
+enum class ETileType : uint8
+{
+	A,
+	B,
+	C,
+	D,
+};
+
+//일단 해보자면..
+UCLASS()
+class WANTED_PROJECT4_API AP4StageGimmck : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AP4StageGimmck();
+
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+	//MapTiles Section
+
+public:
+
+private:
+	//격자 스폰 로직
+	//맵 파츠 A B C D가 원형으로 조립되는데 이걸 기준으로 월드를 위에서 내려다볼때를 기준으로 가상의 격자를 만들어서
+	//이걸 기반으로 맵을 실시간으로 추가 및 삭제로 무한맵 로드및 관리.
+	FIntPoint WorldToCell(const FVector& world) const; //월드 -> 셀인덱스.
+	FVector CellToWorld(const FIntPoint& cell) const; //셀 인덱스 -> 월드위치.
+	void RefreshTiles(); //맵 유지범위 갱신.(이걸 사용하면 현재 플레이어 위치기반으로 맵 로드 및 삭제를 처리하는 위치가 최신화됩니당.
+
+
+	AActor* SpawnTileAt(const FIntPoint& cell); //맵 파츠 스폰.
+
+	TSubclassOf<AActor> PickTileClass(const FIntPoint& cell) const;
+
+	// A B C D 판정
+	static FORCEINLINE ETileType GetCellType(const FIntPoint& cell) //입력된 인자를 기반으로 현재 타일타입 출력.
+	{
+		//홀수 짝수 체크 만약에 true면 홀수 false면 짝수.
+		const bool CheckOddX = (cell.X & 1);
+		const bool CheckOddY = (cell.Y & 1);
+
+		if (CheckOddX == false && CheckOddY == false)
+		{
+			return ETileType::A;
+		}
+
+		if (CheckOddX == true && CheckOddY == false)
+		{
+			return ETileType::B;
+		}
+
+		if (CheckOddX == false && CheckOddY == true)
+		{
+			return ETileType::C;
+		}
+
+		return ETileType::D;
+	}
+public:
+	//맵 타일의 상대좌표(타일 A가 기준점).
+	UPROPERTY(EditAnywhere, Category = Grid)
+	FVector TileOffsetCoordA = FVector::ZeroVector; //타일 A 좌표
+
+	UPROPERTY(EditAnywhere, Category = Grid)
+	FVector TileOffsetCoordB = FVector(9950.f, 3750.0f, -1000.0f); //타일 B 좌표
+
+	UPROPERTY(EditAnywhere, Category = Grid)
+	FVector TileOffsetCoordC = FVector(2800.0f, -3800.0f, -1000.0f); //타일 C 좌표
+
+	UPROPERTY(EditAnywhere, Category = Grid)
+	FVector TileOffsetCoordD = FVector(5000.0f, 0.0f, -1000.0f); //타일 D 좌표
+
+	//UPROPERTY(EditAnywhere, Category = Grid)
+	//FVector TileOffsetCoordD; //타일 D 좌표
+
+	UPROPERTY(EditAnywhere, Category = Grid)
+	int32 GridRadiusX = 2;
+
+	UPROPERTY(EditAnywhere, Category = Grid)
+	int32 GridRadiusY = 1;
+
+
+
+	UPROPERTY(EditAnywhere, Category = Tiles)
+	TSubclassOf<AActor> TileA;
+	UPROPERTY(EditAnywhere, Category = Tiles)
+	TSubclassOf<AActor> TileB;
+	UPROPERTY(EditAnywhere, Category = Tiles)
+	TSubclassOf<AActor> TileC;
+	UPROPERTY(EditAnywhere, Category = Tiles)
+	TSubclassOf<AActor> TileD;
+
+
+
+private:
+	//활성중인 타일 해시로 저장.
+	TMap<FIntPoint, TWeakObjectPtr<AActor>> ActiveTilesMap;
+
+	FIntPoint CurrentCenter = FIntPoint::ZeroValue; // 0,0 기본 시작지점 0,0인 A에 설정.
+
+	FVector Vx;
+	FVector Vy;
+
+};
