e1241630029e75a5cb35a37a4aabea16059a70bf
GyeonHee <hyungi4833@gmail.com>
2025-11-07 16:30:06 +0900

    인벤토리 아이템 사용 구현

---
+++ Source/Wanted_Project4/UI/P4HpBarWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/P4CustomWidget.h"
+#include "P4HpBarWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WANTED_PROJECT4_API UP4HpBarWidget : public UP4CustomWidget
+{
+	GENERATED_BODY()
+	
+};
