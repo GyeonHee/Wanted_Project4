dac902c0387b25793f82913094715be6b26c0d56
승헌 한 <nick501040@gmail.com>
2025-11-12 04:42:07 +0900

    집에서 한 커밋

--- Source/Wanted_Project4/Map/P4StageGimmck.h
+++ Source/Wanted_Project4/Map/P4StageGimmck.h
@@ -79,25 +79,25 @@ private:
 	}
 public:
 	//맵 타일의 상대좌표(타일 A가 기준점).
-	UPROPERTY(VisibleAnywhere, Category = Grid)
+	UPROPERTY(EditAnywhere, Category = Grid)
 	FVector TileOffsetCoordA = FVector::ZeroVector; //타일 A 좌표
 
-	UPROPERTY(VisibleAnywhere, Category = Grid)
+	UPROPERTY(EditAnywhere, Category = Grid)
 	FVector TileOffsetCoordB = FVector(3750.0f, -9950.0, -1000.0f); //타일 B 좌표
 
-	UPROPERTY(VisibleAnywhere, Category = Grid)
+	UPROPERTY(EditAnywhere, Category = Grid)
 	FVector TileOffsetCoordC = FVector(12060.0f, 5410.8f, 100.0f); //타일 C 좌표
 
-	UPROPERTY(VisibleAnywhere, Category = Grid)
+	UPROPERTY(EditAnywhere, Category = Grid)
 	FVector TileOffsetCoordD = FVector(7799.0f, -5025.9f, 0.0f); //타일 D 좌표
 
 	//UPROPERTY(EditAnywhere, Category = Grid)
 	//FVector TileOffsetCoordD; //타일 D 좌표
 
-	UPROPERTY(EditAnywhere, Category = Grid)
-	int32 GridRadiusX = 2;
+	UPROPERTY(VisibleAnywhere, Category = Grid)
+	int32 GridRadiusX = 1;
 
-	UPROPERTY(EditAnywhere, Category = Grid)
+	UPROPERTY(VisibleAnywhere, Category = Grid)
 	int32 GridRadiusY = 1;
 
 
